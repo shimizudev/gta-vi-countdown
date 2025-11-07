f1476437ff337708b8370cf2a513d0d4ea1ba85a
Mohtasim Alam <94981761+shimizudev@users.noreply.github.com>
2025-11-07 22:56:29 +0600

    GTA VI DELAYED AGAIN RAHHHHHH

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,2 +1,2 @@
-export const GTA6_RELEASE_DATE = new Date(Date.UTC(2026, 4, 26));
-// Month is 0-based, so 4 = May
+export const GTA6_RELEASE_DATE = new Date(Date.UTC(2026, 10, 19));
+// Month is 0-based, so 10 = Nov
